65dc00f7614cbdbae2ddfe88672fce98e8807df3
kasun Hasanga <Kasunhasanga96@gmail.com>
2023-11-28 16:20:12 +0530

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by kasun Hasanga on 2023-11-28.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
