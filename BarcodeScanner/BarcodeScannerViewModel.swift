fd4ebc5d95d8e0d414d7924eac1280dc9fa4a2f3
kasun Hasanga <Kasunhasanga96@gmail.com>
2023-12-11 12:34:39 +0530

    complete barcode scanner project

---
+++ BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by kasun Hasanga on 2023-12-11.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
